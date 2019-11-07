3f693a7cc71f282c4437c00472baa38ded4a54db
duc <duc.nguyen80@gmail.com>
2019-11-07 22:54:47 +0100

    add new bienCriteria

---
+++ src/app/views/biens/models/search/bien-critere.ts
@@ -0,0 +1,56 @@
+export interface BienCritere {
+    typeBien: string;
+    consultant: string;
+    cessionBail: boolean;
+    locationPure: boolean;
+    locationDroitEntree: boolean;
+    mursLibres: boolean;
+    mursOccupes: boolean;
+    adresse: string;
+    departement: string;
+    codePostal: string;
+    loyerMin: number;
+    loyerMax: number;
+    prixMin: number;
+    prixMax: number;
+    valeurLocativeMin: number;
+    valeurLocativeMax: number;
+    surfaceTotaleMin: number;
+    surfaceTotaleMax: number;
+    surfaceRDCMin: number;
+    surfaceRDCMax: number;
+    lineaireVitrineMin: number;
+    angle: boolean;
+
+    pointRoute: boolean;
+    pointNoir: boolean;
+    avecPanneau: boolean;
+    sansPasserelles: boolean;
+    licenceIV: boolean;
+    popupStore: boolean;
+    restaurentConduitCheminee: boolean;
+    restaurentSansNuisance: boolean;
+    terrasse: boolean;
+    brouillon: boolean;
+    conclu: boolean;
+    actif: boolean;
+    offreEnCours: boolean;
+    archive: boolean;
+
+    activite: string;
+
+    // Emplacement
+    aDefinir: boolean;
+    numero1: boolean;
+    numero1Alimentaire: boolean;
+    numero1PretAPorter: boolean;
+    rueCommercante: boolean;
+    fluxPieton: boolean;
+    axeVoiture: boolean;
+    ruePietonne: boolean;
+    zoneTouristique: boolean;
+    zoneBureau: boolean;
+    zoneResidentielle: boolean;
+    centreCommercial: boolean;
+    zac: boolean;
+}
